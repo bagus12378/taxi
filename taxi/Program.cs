bf8eabf18ecf68cfbf42a39b0667eca40fe401ea
bagus12378 <bagusk@students.amikom.ac.id>
2023-04-04 20:13:00 -0800

    first commit

---
+++ taxi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            taxi  taxi= new taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPasssanger = "10";
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DroopOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ taxi/taxi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public string NumPasssanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Num Passanger : {0}", NumPasssanger);
+
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} Sedang menjemput penumpang\n", DriverName);
+        }
+
+        public void DroopOffPassanger()
+        {
+            Console.WriteLine("{0} Selesai mengantar penumpang\n", DriverName);
+        }
+    }
+}
+
+
